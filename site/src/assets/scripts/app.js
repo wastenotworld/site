2ca5a3bdfe9aa60e3f1b15d6640ac8ae9547f400
Kevin Green <kevin@ctrlaltdel.world>
2020-06-07 15:36:28 -0400

    more work on the hp, initial drop down thinking/styling

--- site/src/assets/scripts/app.js
+++ site/src/assets/scripts/app.js
@@ -3,6 +3,7 @@ import { picoapp } from 'picoapp'
 import image from './lib/image.js'
 import form from './lib/form.js'
 import pop from './components/pop.js'
+import easy from './components/easy.js'
 
 const state = {
   menuOpen: false
@@ -11,6 +12,7 @@ const state = {
 const components = {
   image,
   pop,
+  easy,
   form
 }
 
---
+++ site/src/assets/scripts/components/easy.js
@@ -0,0 +1,7 @@
+import { component } from 'picoapp'
+import easydropdown from 'easydropdown'
+
+export default component((node) => {
+  console.log('easy')
+  const edd = easydropdown(node)
+})

7451121bf5c2c0293066230f1ef8b1b35bd73f58
Kevin Green <kevin@ctrlaltdel.world>
2020-06-24 16:11:07 -0400

    update to close

--- site/src/assets/scripts/components/listing-card.js
+++ site/src/assets/scripts/components/listing-card.js
@@ -4,7 +4,12 @@ export default component((node) => {
 
   const expanders = node.querySelectorAll('.js-expand-open')
   expanders.forEach(expand => {
-    expand.addEventListener('click', () => node.classList.toggle('open'))
+    expand.addEventListener('click', () => {
+      node.classList.toggle('open')
+      if (node.classList.contains('source')) {
+        node.classList.remove('source')
+      }
+    })
   })
 
   const sourceOpen = node.querySelector('.js-source-open')

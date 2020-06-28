5da0cf34a0ae628bc8e1bb3e30e28ad2334bbb5e
Kevin Green <kevin@ctrlaltdel.world>
2020-06-28 17:15:59 -0400

    switching up the drop downs, thinking about adding search and multi-select for the origin

--- site/src/assets/scripts/components/easy.js
+++ site/src/assets/scripts/components/easy.js
@@ -1,14 +1,42 @@
 import { component } from 'picoapp'
 import easydropdown from 'easydropdown'
+import SlimSelect from 'slim-select'
 
 import { router } from '../index.js'
 
 export default component((node) => {
-  const edd = easydropdown(node, {
-    callbacks: {
-      onOptionClick: value => {
-        router.go(value)
-      }
+  // const edd = easydropdown(node, {
+  //   callbacks: {
+  //     onOptionClick: value => {
+  //       router.go(value)
+  //     },
+  //     onOpen: () => {
+  //       const optionValues = document.querySelectorAll('.edd-option')
+  //       optionValues.forEach(opt => {
+  //         if (!opt.classList.contains('edd-option-disabled')) {
+  //           console.log('not disabled')
+  //           opt.setAttribute('type', 'zag')
+  //           opt.classList.add('taco')
+  //         }
+  //       })
+  //     }
+  //   },
+  //   classNames: {
+  //     rootHasValue: 'waste-value'
+  //   }
+  // })
+
+  // return node => {
+  //   edd.destroy()
+  // }
+  const placeholder = node.getAttribute('data-placeholder')
+  console.log(placeholder)
+  new SlimSelect({
+    select: '#navigationalDrop',
+    showSearch: false,
+    placeholder: placeholder ? placeholder : 'Search Materials',
+    onChange: (info) => {
+      console.log(info)
     }
   })
 })

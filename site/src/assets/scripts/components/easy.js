5566d6029b1df93bd7104a97ae6584cff54c6afb
Kevin Green <kevin@ctrlaltdel.world>
2020-08-31 12:10:11 -0400

    some initial submit form styling, dumbing out all the requested form fields

--- site/src/assets/scripts/components/easy.js
+++ site/src/assets/scripts/components/easy.js
@@ -4,8 +4,9 @@ import SlimSelect from 'slim-select'
 import { router } from '../index.js'
 
 export default component((node) => {
+  console.log(node)
   new SlimSelect({
-    select: '#navigationalDrop',
+    select: node,
     showSearch: false,
     onChange: (info) => {
       console.log(info)

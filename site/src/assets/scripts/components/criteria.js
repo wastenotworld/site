620eb39524a3a6d3d856d273512be2484334089c
Kevin Green <kevin@ctrlaltdel.world>
2020-09-04 17:12:10 -0400

    criteria guarantee open

--- site/src/assets/scripts/components/criteria.js
+++ site/src/assets/scripts/components/criteria.js
@@ -2,4 +2,8 @@ import { component } from 'picoapp'
 
 export default component((node) => {
   console.log('criteria')
+  const gButton = node.querySelector('.js-guarentee')
+  gButton.addEventListener('click', () => {
+    node.classList.toggle('open')
+  })
 })

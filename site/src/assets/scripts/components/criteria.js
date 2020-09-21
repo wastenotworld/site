4952efc7c73a15c2edb6b799956fe4f9143cb9eb
Kevin Green <kevin@ctrlaltdel.world>
2020-09-21 11:32:51 -0400

    add the ability to always close the guarantee module experience

--- site/src/assets/scripts/components/criteria.js
+++ site/src/assets/scripts/components/criteria.js
@@ -1,8 +1,14 @@
 import { component } from 'picoapp'
 
 export default component((node) => {
-  const gButton = node.querySelector('.js-guarentee')
-  gButton.addEventListener('click', () => {
-    node.classList.toggle('open')
+  const gButtons = node.querySelectorAll('.js-guarantee')
+  const closeG = node.querySelector('.js-close-guarantee')
+  gButtons.forEach(b => {
+    b.addEventListener('click', () => {
+      node.classList.toggle('open')
+    })
+  })
+  closeG.addEventListener('click', () => {
+    node.classList.remove('open')
   })
 })
